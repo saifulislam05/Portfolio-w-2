cd4669a1fdfabfbcae20c459b02a1e85276620ea
saifulislam05 <saiful.bijulia@gmail.com>
2024-01-03 22:43:08 +0530

    fixed some issue

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
       <div className="sidebar-info">
         <figure className="avatar-box">
           <img
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Saiful Islam"
           />
         </figure>
